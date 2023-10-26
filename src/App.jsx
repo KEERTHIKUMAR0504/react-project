2664644bb07daf26214359de14cb1ffe642e67a0
KEERTHIKUMAR0504 <tskeerthikumar0504@gmail.com>
2023-10-26 18:22:03 +0530

    codes reduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import './App.css'
+import { useReducer } from "react";
+import "./App.css";
 
 export default function App() {
+  const users = [
+    {
+      name: "Sivakumar",
+      pic: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
+    },
+    {
+      name: "Valarmathi",
+      pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwHvEGamFasFcudDxAlTzNzd2ZsSRKsmf4Uw&usqp=CAU",
+    },
+    {
+      name: "Keerthi",
+      pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUKPmu8M0apKcy1oABXStYt3eHAbDCoLsC9OASEYxJXCaDMmSy3IYnujBENMZXQznsSNY&usqp=CAU",
+    },
+  ];
   return (
     <div className="App">
-      <Welcome name="Sivakumar" pic="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"/>
-      <Welcome name="Valarmathi" pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwHvEGamFasFcudDxAlTzNzd2ZsSRKsmf4Uw&usqp=CAU"/>
-      <Welcome name="Keerthi" pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUKPmu8M0apKcy1oABXStYt3eHAbDCoLsC9OASEYxJXCaDMmSy3IYnujBENMZXQznsSNY&usqp=CAU"/>
+      {users}.map((usr) = (
+      <Welcome name={usr.name} pic={usr.pic} />
+      ))
     </div>
   );
 }
 
-function Welcome({name,pic}){
-  // const {name, pic} = props;
-  return(
-    <div>
-      <img alt={name} className="pro-pic" src={pic}/>
-      <h1>Hello,{name}😎✔ </h1>
-    </div>
-  );
-}
+// function Welcome({name,pic}){
+//   // const {name, pic} = props;
+//   return(
+//     <div>
+//       <img alt={name} className="pro-pic" src={pic}/>
+//       <h1>Hello,{name}😎✔ </h1>
+//     </div>
